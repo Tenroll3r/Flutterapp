a7eadf6c8cbb7f025e74bbb343c217a30a986467
Guilherme Tenroller <60934581+Tenroll3r@users.noreply.github.com>
2020-02-18 10:55:51 -0300

    adcional para mexer com json

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // Codigo abaixo inicia a aplicação chamando o widget da Homepage.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter App',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: HomePage(),
     );
   }
 }
 
-  class HomePage extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Container(
-        
-      );
-    }
+// HomePage
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All in One"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Olá mundo"),
+        ),
+      ),
+    );
   }
+}
